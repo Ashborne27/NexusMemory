f3978121a70bae1a8209046201676f7e24b8552d
NexusMaestro <maestro@nexus.core>
2026-08-22 16:02:00 +0000

    NexusMemory - Fix Java syntax order in MainActivity and ensure service launch

--- android/app/src/main/java/com/nexusmemory/app/MainActivity.java
+++ android/app/src/main/java/com/nexusmemory/app/MainActivity.java
@@ -1,7 +1,21 @@
-import android.content.Intent;
-import com.nexus.memory.NexusForegroundService;
 package com.nexusmemory.app;
 
+import android.os.Bundle;
+import android.content.Intent;
 import com.getcapacitor.BridgeActivity;
+import com.nexus.memory.NexusForegroundService;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Lancement souverain du service foreground pour la persistance et la notification
+        Intent serviceIntent = new Intent(this, NexusForegroundService.class);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            startForegroundService(serviceIntent);
+        } else {
+            startService(serviceIntent);
+        }
+    }
+}
